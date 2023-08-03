7f2a9818a7c9c589bda7b6ce744a9b482f022744
iOSDevLime <onthelots@naver.com>
2023-08-04 02:34:47 +0900

    Create Insight Project files

---
+++ Insight/Insight/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Insight
+//
+//  Created by Jae hyuk Yim on 2023/08/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
+
