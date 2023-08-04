3b830551e9e69dec2d49e11a9d4ecd25c2fea147
iOSDevLime <onthelots@naver.com>
2023-08-04 16:55:49 +0900

    앱 명칭 변경에 따른 프로젝트 파일 재 설치

---
+++ Dangle/Dangle/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Dangle
+//
+//  Created by Jae hyuk Yim on 2023/08/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemRed
+  }
+}
+
---
+++ Dangle/DangleUITests/DangleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DangleUITestsLaunchTests.swift
+//  DangleUITests
+//
+//  Created by Jae hyuk Yim on 2023/08/04.
+//
+
+import XCTest
+
+final class DangleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
